cb62f480f4841700cc39530c83bc934967d66b10
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-02-11 22:55:45 -0800

    Change VM name and memory allocation

--- main.tf
+++ main.tf
@@ -32,12 +32,12 @@ data "vsphere_virtual_machine" "ubuntu" {
 }
 
 resource "vsphere_virtual_machine" "learn" {
-  name             = "learn-terraform"
+  name             = "learn-terraform-doubled"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
-  memory   = 1024
+  memory   = 2048
 
   network_interface {
     network_id = data.vsphere_network.network.id
